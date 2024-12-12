c1c3e1a8757331fc367a6e6977dbf50740f5be14
Kudláčková, Tereza <kudlate1@fel.cvut.cz>
2024-12-12 13:03:51 +0100

    working with mutagenesis dataset

--- julia/scripts/datatraining.jl
+++ julia/scripts/datatraining.jl
@@ -1,17 +1,21 @@
 using Random, Distances, Flux
 using Plots
+
 using Mill
 using HSTreeDistance
+
+using JsonGrinder
+using JSON3
 
 include("../src/triplet-loss.jl")
 include("../src/dataloading.jl")
 
 
-X = Float64.([1 3 5 7 9 2 4 6 8 10; 2 2 2 2 2 3 3 3 3 3])
-y = [1 1 1 1 1 0 0 0 0 0]
+# X = Float64.([1 3 5 7 9 2 4 6 8 10; 2 2 2 2 2 3 3 3 3 3])
+# y = [1 1 1 1 1 0 0 0 0 0]
 
-PN = ProductNode((x = Array(X[1, :]'), y  = Array(X[2, :]')))
-product_nodes = [PN[i] for i in 1:10]
+# PN = ProductNode((x = Array(X[1, :]'), y  = Array(X[2, :]')))
+# product_nodes = [PN[i] for i in 1:10]
 
 # pn = product_nodes[1]
 # pn.data.x.data |> only
@@ -45,24 +49,25 @@ function plotData(points, labels)
     )
 end
 
-function train(method::SelectingTripletMethod, product_nodes; λ = 0.01, max_iter = 200)
+function train(method::SelectingTripletMethod; λ = 0.01, max_iter = 200)
 
-    metric = reflectmetric(product_nodes[1], weight_transform=softplus)
+    X, y = load("julia/data/mutagenesis.json")
+    metric = reflectmetric(X[1], weight_transform=softplus)
     ps = Flux.params(metric)
     opt = Descent(λ)
 
     for iter in 1:max_iter
 
-        triplet = selectTriplet(method, product_nodes, y, metric)
-        (triplet == nothing) && break 
+        triplet = selectTriplet(method, X, y, metric)
+        (triplet === nothing) && break 
 
         anchor, pos, neg = triplet
-        (pos == nothing || neg == nothing) && continue
+        (pos === nothing || neg === nothing) && continue
 
         loss, grad = Flux.withgradient(() -> tripletLoss(anchor, pos, neg, metric), ps)
         Flux.update!(opt, ps, grad)
 
-        println("Epoch $iter, loss $loss, [w1,w2] = $ps")
+        println("Iteration $iter, loss $loss, params = $ps")
     end
 
     return w
@@ -70,15 +75,12 @@ end
 
 #-----------------------------------------------------------------------------------------------------------
 
-using JsonGrinder
-using JSON3
+# X, y = load("julia/data/mutagenesis.json")
 
-X, y = load("julia/data/mutagenesis.json")
-
-metric = reflectmetric(X[1])
-metric(X[1], X[2])
-Flux.params(metric)
+# metric = reflectmetric(X[1])
+# metric(X[1], X[2])
+# Flux.params(metric)
 
 # original dataset
 plotData(product_nodes, y)
-w = train(SelectHard(), product_nodes)  
+w = train(SelectHard())
