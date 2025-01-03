abdca0bd40e9a3b56434a4d994a33db0daf1eb56
Kudláčková, Tereza <kudlate1@fel.cvut.cz>
2025-01-03 11:32:42 +0100

    new files adding

---
+++ julia/tests/class-split-test.jl
@@ -0,0 +1,72 @@
+using Test
+
+function testClassSplit()
+
+    @testset "Testing splitClasses function" begin
+        
+        @testset "Testing basic functionality" begin
+        
+            product_nodes = ["node1", "node2", "node3", "node4"]
+            y = ["A", "B", "A", "A"]
+            anchor = "node1"
+            anchor_label = "A"
+
+            positives, negatives = separateClasses(product_nodes, y, anchor, anchor_label)
+
+            expected_positives = ["node3", "node4"]
+            expected_negatives = ["node2"]
+
+            @test positives == expected_positives
+            @test negatives == expected_negatives
+        end
+        
+        @testset "Testing edge case: only one node in a batch" begin
+            
+            product_nodes = ["node1"]
+            y = ["A"]
+            anchor = "node1"
+            anchor_label = "A"
+
+            positives, negatives = separateClasses(product_nodes, y, anchor, anchor_label)
+
+            expected_positives = []
+            expected_negatives = []
+
+            @test positives == expected_positives
+            @test negatives == expected_negatives
+        end
+
+        @testset "Testing edge case: no positives nodes in a batch" begin 
+
+            product_nodes = ["node1", "node2", "node3", "node4"]
+            y = ["A", "B", "A", "A"]
+            anchor = "node2"
+            anchor_label = "B"
+            positives, negatives = separateClasses(product_nodes, y, anchor, anchor_label)
+
+            expected_positives = []
+            expected_negatives = ["node1", "node3", "node4"]
+
+            @test positives == expected_positives
+            @test negatives == expected_negatives
+        end
+
+        @testset "Testing edge case: no negatives nodes in a batch" begin 
+
+            product_nodes = ["node1", "node2", "node3", "node4"]
+            y = ["A", "A", "A", "A"]
+            anchor = "node2"
+            anchor_label = "A"
+            positives, negatives = separateClasses(product_nodes, y, anchor, anchor_label)
+    
+            expected_positives = ["node1", "node3", "node4"]
+            expected_negatives = []
+    
+            @test positives == expected_positives
+            @test negatives == expected_negatives
+        end
+    end
+end
+
+testClassSplit();
+
---
+++ julia/tests/distance-test.jl
@@ -0,0 +1,59 @@
+using Test
+
+
+function testDistance()
+
+    X = Float64.([1 -3 0 0 1 2 4 6 0 0; 2 4 0 1 1 3 -3 5 0 2])
+    PN = ProductNode((x = Array(X[1, :]'), y  = Array(X[2, :]')))
+    product_nodes = [PN[i] for i in 1:10]
+    PM = reflectmetric(product_nodes[1])
+
+    @testset "Testing correct distance computation" begin
+        
+        @testset "Testing basic case" begin
+            @test distance(PN[3], PN[4], PM) == 1.0
+            @test distance(PN[3], PN[10], PM) == 2.0
+        end
+
+        @testset "Testing distance of the two same nodes" begin
+            @test -1e-9 < distance(PN[1], PN[1], PM) < 1e-9
+            @test -1e-9 < distance(PN[2], PN[2], PM) < 1e-9
+            @test -1e-9 < distance(PN[9], PN[9], PM) < 1e-9
+        end
+
+        @testset "Testing precision" begin
+            @test distance(PN[3], PN[5], PM) ≈ sqrt(2.0)
+        end
+
+    end
+
+    @testset "Testing pairwise matrix" begin
+        
+        @testset "Testing correct output type of pairwiseDistance function" begin
+            matrix1 = pairwiseDistance(product_nodes)
+            matrix2 = pairwiseDistance([product_nodes[1]])
+            matrix3 = pairwiseDistance([])
+            @test matrix1 |> typeof == Matrix{Float64}
+            @test matrix2 |> typeof == Matrix{Float64}
+            @test matrix3 |> typeof == Matrix{Float64}
+        end
+
+        @testset "Testing correct computation of matrix" begin
+            X = Float64.([0 0 0; 1 2 3])
+            PN = ProductNode((x = Array(X[1, :]'), y  = Array(X[2, :]')))
+            product_nodes = [PN[i] for i in 1:3]
+            @test round.(pairwiseDistance(product_nodes; wt=identity), digits=5) ≈ [0.0 1.0 2.0;
+                                                                                    1.0 0.0 1.0;
+                                                                                    2.0 1.0 0.0]
+            X = Float64.([0 0 0; 1 2 3])
+            PN = ProductNode((x = Array(X[1, :]'), y  = Array(X[2, :]')))
+            product_nodes = [PN[i] for i in 1:3]
+            @test round.(pairwiseDistance(product_nodes; wt=softplus), digits=5) ≈ [0.0      1.14598  2.29195;
+                                                                                    1.14598  0.0      1.14598;
+                                                                                    2.29195  1.14598  0.0]                                              
+        end
+    end
+end
+
+testDistance();
+
