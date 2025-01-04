a90f9836a2e0a108b262d728e3343f82e611fcbe
Kudláčková, Tereza <kudlate1@fel.cvut.cz>
2025-01-04 17:04:38 +0100

    triplet selection test

---
+++ julia/tests/triplet-test.jl
@@ -0,0 +1,35 @@
+using Test
+
+function testTripletSelection()
+
+    X = Float64.([1 -3 0 0 1 2 4 6 0 0; 2 4 0 1 1 3 -3 5 0 2])
+    PN = ProductNode((x = Array(X[1, :]'), y  = Array(X[2, :]')))
+    product_nodes = [PN[i] for i in 1:10]
+    PM = reflectmetric(product_nodes[1])
+    dists = pairwiseDistance(product_nodes)
+
+    @testset "Testing selectTriplet function" verbose=true begin
+
+        @testset "Testing basic case of return" begin
+            y = [1 1 1 1 1 0 0 0 0 0]
+            @test (selectTriplet(SelectRandom(), dists, product_nodes, y, PM)) |> typeof <: Tuple{ProductNode, ProductNode, ProductNode}
+            @test (selectTriplet(SelectHard(), dists, product_nodes, y, PM)) |> typeof <: Tuple{ProductNode, ProductNode, ProductNode}
+        end
+
+        @testset "Testing edge case: negative not found" begin
+            # possible to choose a batch of just one class -> triplet is incomplete, should be treated in other funcs
+            y = [1 1 1 1 1 1 1 1 1 1]
+            @test (selectTriplet(SelectRandom(), dists, product_nodes, y, PM)) |> typeof <: Tuple{ProductNode, ProductNode, Bool}
+            @test (selectTriplet(SelectHard(), dists, product_nodes, y, PM)) |> typeof <: Tuple{ProductNode, ProductNode, Bool}
+        end
+
+        @testset "Testing edge case: positive not found" begin
+            # both SelectHard() and SelectRandom() repeats anchor selecting -> returns always complete triplet 
+            y = [0 1 1] # if anchor = 0 -> positive class is empty
+            @test (selectTriplet(SelectRandom(), dists, product_nodes[1:3], y, PM)) |> typeof <: Tuple{ProductNode, ProductNode, ProductNode}
+            @test (selectTriplet(SelectHard(), dists, product_nodes[1:3], y, PM)) |> typeof <: Tuple{ProductNode, ProductNode, ProductNode}
+        end
+    end
+end
+
+testTripletSelection();
