79159e7525ea5c7c08eb4df906478056caecff85
Kudláčková, Tereza <kudlate1@fel.cvut.cz>
2025-03-25 11:03:51 +0100

    EM refactor

--- src/em.jl
+++ src/em.jl
@@ -1,9 +1,8 @@
 function init_params(d::Int, k::Int)
 
-    Random.seed!(42)
-    μ = X[:, rand(1:n, K)]
-    Σ = [Matrix(I, d, d) for _ in 1:K]
-    π = fill(1/K, K)
+    μ = X[:, rand(1:n, k)]
+    Σ = [Matrix(I, d, d) for _ in 1:k]
+    π = fill(1/k, k)
 
     return μ, Σ, π
 end
@@ -20,33 +19,47 @@ function gaussian(xₙ, μₖ, Σₖ)
     xₙ, μₖ, Σₖ: initialized parameters
     """
 
-    d = size(xₙ, 2)
+    d = size(xₙ, 1)
+
     frac1 = 1 / (2π)^(d/2)
     frac2 = 1 / sqrt(det(Σₖ))
     _exp = -0.5 * (xₙ - μₖ)' * inv(Σₖ) * (xₙ - μₖ)
+
     return frac1 * frac2 * exp(_exp)
 end
 
-function compute_responsibilities(X, π, μ, Σ, γ, n, k)
+function compute_responsibilities(X, μ, Σ, π, γ)
+
+    n = size(X, 2)
+    k = size(μ, 2)
 
     for i in 1:n
         total_prob = 0.0
         for j in 1:k
-            γ[i, j] = π[j] * gaussian_pdf(X[:, i], μ[:, j], Σ[j])
+            γ[i, j] = π[j] * gaussian(X[:, i], μ[:, j], Σ[j])
             total_prob += γ[i, j]
         end
         γ[i, :] ./= total_prob
     end
+
+    return γ
 end
 
-function update_means(X, μ, γ, k)
+function update_means(X, μ, γ, N)
+
+    k = size(μ, 2)
 
     for j in 1:k
         μ[:, j] = sum(X .* γ[:, j]', dims=2) / N[j]
     end
+
+    return μ
 end
 
-function update_covariances(X, μ, Σ, γ, N, n, k)
+function update_covariances(X, μ, Σ, γ, N)
+
+    d, n = size(X)
+    k = size(μ, 2)
 
     for j in 1:k
         Σ[j] = zeros(d, d)
@@ -56,9 +69,26 @@ function update_covariances(X, μ, Σ, γ, N, n, k)
         end
         Σ[j] /= N[j]
     end
+
+    return Σ
 end
 
-function EM_GMM(X, k::Int; max_iter::Int=100, atol=1e-4)
+function EM_GMM(X, k::Int; max_iter::Int=100)
+    """
+    Performs EM algorithm for gaussian mixtures, more details in 
+    Bishop - Pattern Recognition and Machine Learning, 2006
+
+    Params:
+    X (Matrix):     observation points
+    k (Int):        the number of clusters
+    max_iter (Int): max iterations
+
+    Return:
+    μ (Matrix): learned means of the clusters
+    Σ (Matrix): learned covariance matrices of the clusters
+    π (Vector): learned mixing coefficients
+    γ (Matrix): learned posterior probabilities
+    """
 
     d, n = size(X)  # dims, points
 
@@ -70,17 +100,17 @@ function EM_GMM(X, k::Int; max_iter::Int=100, atol=1e-4)
     for _ in 1:max_iter
 
         # 2. E-Step: compute responsibilities 
-        γ = compute_responsibilities(X, π, μ, Σ, γ, n, k)
+        γ = compute_responsibilities(X, μ, Σ, π, γ)
 
         # 3. M-Step: update parameters
         N = sum(γ, dims=1)[:]
-        μ = update_means(X, μ, γ, k)
-        Σ = update_covariances(X, μ, Σ, γ, N, n, k)
+        μ = update_means(X, μ, γ, N)
+        Σ = update_covariances(X, μ, Σ, γ, N)
         π = N / n
 
         # 4. eval log likelihood + convergence check
         log_likelihood = sum(log(sum(π[k] * gaussian_pdf(X[:, i], μ[:, k], Σ[k]) for k in 1:K)) for i in 1:n)
-        abs(log_likelihood - log_likelihood_old) < atol && break
+        (abs(log_likelihood - log_likelihood_old) < 1e-4) && break
         loglike_init = log_likelihood
     end
 
